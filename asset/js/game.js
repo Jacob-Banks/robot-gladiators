3ee0ccbb35ba2bba200dd3d678216e6e8fdb453c
Jacob Banks <kobybanks@gmail.com>
2021-05-29 13:04:06 -0400

    named 2 robots

--- asset/js/game.js
+++ asset/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
+function fight() {
+  window.alert("The fight has begun!");
+}
+//fight();
