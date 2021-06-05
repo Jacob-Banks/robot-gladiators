f46e0352074db62974b906044d1204a441f244b9
Jacob Banks <kobybanks@gmail.com>
2021-06-04 22:58:06 -0400

    shop function

--- asset/js/game.js
+++ asset/js/game.js
@@ -26,14 +26,52 @@ var fight = function () {
     for (var i = 0; i < enemyNames.length; i++) {
       ...
     }
-  
+  // if player is still alive and we're not at the last enemy in the array
+if (playerHealth > 0 && i < enemyNames.length - 1) {
+  shop();
+}
   // check enemy's health
   if (enemyHealth <= 0) {
     window.alert(enemyName + " has died!");
   } else {
     window.alert(enemyName + " still has " + enemyHealth + " health left.");
   }
-
+  switch (shopOptionPrompt) {
+    case "REFILL": // new case
+    case "refill":
+      if (playerMoney >= 7) {
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+  
+        playerHealth = playerHealth + 20;
+        playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You don't have enough money!");
+      }
+  
+      break;
+    case "UPGRADE": // new case
+    case "upgrade":
+      if (playerMoney >= 7) {
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+  
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You don't have enough money!");
+      }
+  
+      break;
+    case "LEAVE": // new case
+    case "leave":
+      window.alert("Leaving the store.");
+      break;
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+      shop();
+      break;
+  }
   // remove players's health by subtracting the amount set in the enemyAttack variable
   playerHealth = playerHealth - enemyAttack;
   console.log(
